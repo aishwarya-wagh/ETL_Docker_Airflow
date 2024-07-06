423abd934e93fcacf66c8e9dafa70635333367a1
Aishwarya-University-of-waterloo <aswagh@uwaterloo.ca>
2024-07-06 12:21:30 -0400

    data cleaning

---
+++ DAG/Data_cleaning.py
@@ -0,0 +1,27 @@
+# Import Libraries
+import numpy as numpy
+import pandas as pd
+pd.set_option('display.max_columns',100)
+
+# Read data
+df = pd.read_csv('/opt/airflow/dags/Wine.csv')
+#print(df.head()) # Successful
+
+# Data Quality Analysis
+
+### Data Type Check
+print('Dataframe dimensions:',df.shape)
+print(df.info())
+
+### 1. Missing value analysis
+# print(df.isna().sum())
+
+# Data Analysis
+print(df.describe())
+
+# Add index column
+df['id'] = [i for i in range(len(df))]
+print(df.info())
+
+# Store new csv in the dags directory
+df.to_csv('/opt/airflow/dags/wine_cleaned.csv',index=False)
